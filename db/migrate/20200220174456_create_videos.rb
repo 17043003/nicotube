61077531c767b609de98fb6ce37bbca087930fd0
17043003 <35760051+17043003@users.noreply.github.com>
2020-02-21 06:04:00 +0900

    migrate videos table

--- db/migrate/20200220174456_create_videos.rb
+++ db/migrate/20200220174456_create_videos.rb
@@ -2,8 +2,9 @@ class CreateVideos < ActiveRecord::Migration[5.2]
   def change
     create_table :videos do |t|
       t.string :name
-      t.date_time :watched_at
+      t.datetime :watched_at
       t.integer :watch_time
+      t.string :videoId
 
       t.timestamps
     end
