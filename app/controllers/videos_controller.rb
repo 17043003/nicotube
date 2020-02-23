31ba00b3d108fe11842d39809a45798c5889cc81
17043003 <35760051+17043003@users.noreply.github.com>
2020-02-24 04:57:04 +0900

    improve videos view

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -10,12 +10,12 @@ class VideosController < ApplicationController
     type = params[:type]
     @nico_end_point = "https://api.search.nicovideo.jp/api/v2/#{type}/contents/search?"
 
-    word = params[:word] # 検索ワード
+    @word = params[:word] # 検索ワード
     number_of_content = 10 # 取得する動画数
     number_of_offset = 0 # 先頭から何番目の動画から取得するか
 
     data = {
-    "q": word,
+    "q": @word,
     "targets": "title,description,tags",
     "_sort": "-startTime",
     "fields": "contentId,title,description,tags,thumbnailUrl",
@@ -25,14 +25,11 @@ class VideosController < ApplicationController
 
     query = data.to_query
     uri = URI(@nico_end_point+query)
-    # http = Net::HTTP.new(uri.host, uri.port)
-    # http.use_ssl = true
 
     response = Net::HTTP.get_response(uri)
     response_body = JSON.parse(response.read_body)
 
     if response_body['data'].present?
-      # @contentId = response_body['data'][0]['contentId']
       @content = response_body['data'].map do |response_data|
         {
           contentId: response_data['contentId'],
@@ -43,14 +40,8 @@ class VideosController < ApplicationController
     else
       flash[:notice] = "動画が見つかりませんでした"
     end
-
-    # p response.read_body.class
-    # p response_body['data'][0]['title']
-    p @content[0]
-    p response_body['data'].length
   end
 
   def show
-    p params[:id]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,4 @@ Rails.application.routes.draw do
   root 'videos#top'
   get 'videos/search'
   get "videos/:videoId" => 'videos#show', param: :videoId
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
